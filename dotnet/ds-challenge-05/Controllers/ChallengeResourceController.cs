cb62fc64a9ccd6858e95f1a63d9dd1ab9bb75989
Phillip Knezevich <phknezev@redhat.com>
2024-11-20 13:38:30 -0500

    added challenge5

---
+++ dotnet/ds-challenge-05/Controllers/ChallengeResourceController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ds_challenge_05.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ChallengeResourceController : ControllerBase
+{
+    private readonly ILogger<ChallengeResourceController> _logger;
+
+    public ChallengeResourceController(ILogger<ChallengeResourceController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("challenge")]
+    [Produces("text/plain")]
+    public string ChallengeMethod()
+    {
+        string name = "OpenShift DevSpaces";
+        int length = name.Length;
+
+        for (int index = 0; index < length; index++)
+        {
+            char indexChar = name[index];
+            if (index == 4) // Index 4 is the fifth character in a zero-based index system
+            {
+                char fifthCharacter = indexChar;
+                return $"The Fifth Character in the word \"OpenShift\"=[{fifthCharacter}]\n";
+            }
+        }
+
+        return "String is shorter than length 5 \n";
+    }
+
+    [HttpGet("greet/{name}")]
+    [Produces("text/plain")]
+    public string GreetUser(string name)
+    {
+        string user = name;
+        return $"Hello {user}\n";
+    }
+
+    [HttpGet("hello")]
+    [Produces("text/plain")]
+    public string Greet()
+    {
+        return "Hello Challenge Attendees";
+    }
+}
